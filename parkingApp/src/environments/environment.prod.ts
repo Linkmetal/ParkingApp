ffbd81fc93949c71047e25b5326bf23545ebfb5b
carlosdiaz <carlosdiaz@itop.es>
2019-10-06 04:21:02 +0100

    cambiadas envs

--- parkingApp/src/environments/environment.prod.ts
+++ parkingApp/src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   // server: 'http://192.168.1.5:3000/api',
   server: 'http:///52.56.223.39:3000/api',
-  profileServer: 'https://parkingmvc.azurewebsites.net/api'
+  profileServer: 'https://parkingmvc.azurewebsites.net/api',
+  language: 'es'
+  // profileServer: 'https://localhost:44322/api'
 };
